7de662e2f7692fbf1bf25afe2fcea61404c04d58
tanasri <tanawan95@gmail.com>
2024-03-13 14:08:13 +0700

    feat:select answer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,15 @@ import { useReducer } from "react";
 import Loader from "./Loader.jsx";
 import Error from "./Error.jsx";
 import StartScreen from "./StartScreen.jsx";
+import Question from "./Question.jsx";
 
 const initialState = {
   questions: [],
   //'loading','error','ready','active','finish'
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -18,13 +22,28 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points: question.correctOption
+          ? state.points + question.points
+          : state.points,
+      };
     default:
       throw new Error("action unknow");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestion = questions.length;
 
@@ -41,7 +60,16 @@ export default function App() {
       <Body>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestion} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestion} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Body>
     </div>
   );
